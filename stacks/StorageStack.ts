153e572d888a8082b39bcf8214c9134307e3bf36
chineduede <chndvz@gmail.com>
2024-01-24 12:43:42 +0100

    add storage stack

---
+++ stacks/StorageStack.ts
@@ -0,0 +1,15 @@
+import { Bucket, StackContext, Table } from "sst/constructs";
+
+export function StorageStack(scope: StackContext) {
+  const table = new Table(scope.stack, "Notes", {
+    fields: {
+      userId: "string",
+      noteId: "string",
+    },
+    primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
+  });
+
+  const bucket = new Bucket(scope.stack, "Uploads");
+
+  return { table, bucket };
+}
